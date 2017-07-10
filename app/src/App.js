4fafb2c177ebf674692532e845a4740a37ecf3bb
chompy18 <sefi@tikalk.com>
2017-07-10 16:19:13 +0300

    header integration

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
-import Siderbar from './components/Siderbar';
-import Resource from './components/Resource';
+import Siderbar from './Components/Siderbar';
+import Resource from './Components/Resource';
 import {observer} from "mobx-react";
 
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
@@ -15,13 +15,29 @@ class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				<div className="header"></div>
+				<t-header ref={(e) => { this.tHeader = e; }} logo-name="logoName" logo-url="http://pach.com" user-url="http://avatar.jpg" user-name="Sefi"></t-header>
 				<div className="flex-container">
 					<Siderbar />
 					<Resource />
 				</div>
 			</div>
 		);
+	}
+
+	componentDidMount() {
+		this.tHeader.messages = [
+			{
+				avatarUrl: 'https://avatars1.githubusercontent.com/u/681169?v=3',
+				text: 'this is the message 123',
+				date: '27/8/16',
+				isNew: true
+			},
+			{
+				avatarUrl: 'https://avatars1.githubusercontent.com/u/681169?v=3',
+				text: 'another message another message 56747',
+				date: '26/8/16',
+				isNew: false
+			}];
 	}
 }
 
--- app/src/index.js
+++ app/src/index.js
@@ -4,5 +4,10 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import "slim-js";
+import "webco/components/t-header";
+import "webco/components/t-message-box";
+import "webco/components/t-message";
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
